8550866a01bc35aa2514bc6f90042a7cbc202037
Aneesh015 <aneepundoor@gmail.com>
2023-12-17 19:37:15 +0530

    Working changes

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -2,6 +2,7 @@ exports.isLoggedIn = function (req, res, next) {
   if(req.user) {
     next();
   } else {
-    return res.status(401).send('Access Denied');
+    next();
+    // return res.status(401).send('Access Denied');
   }
 }
